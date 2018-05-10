3a9f31c5f7a6f3d7bf4bdd455b467b6baf8f3800
russlex <33608766+russlex@users.noreply.github.com>
2018-05-10 13:08:35 +0200

    Update index.js

--- src/components/crowdsale/index.js
+++ src/components/crowdsale/index.js
@@ -162,17 +162,12 @@ export class Crowdsale extends React.Component {
             </div>
           </div>
           <div className="total-funds-chart-container">
-            <div className="total-funds-chart-division"/>
             <div className="total-funds-chart-division">
-               <p className="description">Soft Cap</p>
+               <p className="soft">Soft Cap</p>
             </div>
-            <div className="total-funds-chart-division"/>
-            <div className="total-funds-chart-division"/>
-            <div className="total-funds-chart-division"/>
-            <div className="total-funds-chart-division"/>
-            <div className="total-funds-chart-division"/>
-            <div className="total-funds-chart-division"/>
-            <div className="total-funds-chart-division"/>
+            <div className="total-funds-chart-division">
+               <p className="sale">Pre Sale</p>
+            </div>
             <div className="total-funds-chart">
               <div className="total-funds-chart-active" style={{ width: `${tokensClaimedRatio}%` }}/>
             </div>
